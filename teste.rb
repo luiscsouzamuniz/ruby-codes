61bd128fe48ee908bc3ce937025e08ef63393f11
Luis Carlos de Souza <luisc.souzamuniz@gmail.com>
2018-12-06 01:05:52 -0200

    teste

---
+++ teste.rb
@@ -0,0 +1,22 @@
+puts "Insira seu nome: "
+name = gets.chomp.to_s
+puts   '
+Não incomode o webcorno '+name+'
+/                       \
+/X/                       \X\
+|XX\         _____         /XX|
+|XXX\     _/       \_     /XXX|___________
+\XXXXXXX             XXXXXXX/            \\\
+  \XXXX    /     \    XXXXX/                \\\
+       |   0     0   |                         \
+        |           |                           \
+         \         /                            |______//
+          \       /                             |
+           | O_O | \                            |
+            \ _ /   \________________           |
+                       | |  | |      \         /
+                       / |  / |       \______/
+                       \ |  \ |        \ |  \ |
+                     __| |__| |      __| |__| |
+                     |___||___|      |___||___|
+                                         '
